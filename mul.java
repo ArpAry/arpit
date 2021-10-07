ff19e0a216d95cf96b02f6c372ca8eab5f4a23ab
Arpit Arya <87825317+ArpAry@users.noreply.github.com>
2021-10-07 23:40:22 +0530

    Add files via upload

---
+++ mul.java
@@ -0,0 +1,14 @@
+package arp30;
+import java.util.Scanner;
+
+public class mul {
+public static void main(String [] args)
+{
+	Scanner a=new Scanner(System.in);
+	int b,d,e;
+	b=a.nextInt();
+	d=a.nextInt();
+	e=b*d;
+	System.out.print("multiply of "+b+"and"+d+" =="+e);
+}
+}
